4d2e0630637818a1edcab102c3c2c39e74cdbb4f
Hayden Westfall <westfallhayden@gmail.com>
2024-06-02 20:46:24 -0400

    Adds sitemap and new webpack plugin to copy it to dist on build

--- webpack.config.js
+++ webpack.config.js
@@ -1,114 +1,118 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const PATHS = {
-  src: path.join(__dirname, 'src'),
-  img: path.join(__dirname, 'src/img'),
-  styles: path.join(__dirname, 'src/styles'),
-  build: path.join(__dirname, 'build')
-}
+  src: path.join(__dirname, "src"),
+  img: path.join(__dirname, "src/img"),
+  styles: path.join(__dirname, "src/styles"),
+  build: path.join(__dirname, "build"),
+};
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   resolve: {
-    alias: { "barba": path.resolve('node_modules', '@barba/core/dist/barba.js'), }
+    alias: { barba: path.resolve("node_modules", "@barba/core/dist/barba.js") },
   },
   entry: {
-    common: path.resolve(__dirname, 'src/common.js'),
-    index: path.resolve(__dirname, 'src/route_home/index.js'),
-    about: path.resolve(__dirname, 'src/route_about/about.js'),
-    testimonies: path.resolve(__dirname, 'src/route_testimonies/testimonies.js'),
-    packages: path.resolve(__dirname, 'src/route_packages/packages.js'),
-    journal: path.resolve(__dirname, 'src/route_journal/journal.js'),
-    contact: path.resolve(__dirname, 'src/route_contact/contact.js'),
+    common: path.resolve(__dirname, "src/common.js"),
+    index: path.resolve(__dirname, "src/route_home/index.js"),
+    about: path.resolve(__dirname, "src/route_about/about.js"),
+    testimonies: path.resolve(
+      __dirname,
+      "src/route_testimonies/testimonies.js"
+    ),
+    packages: path.resolve(__dirname, "src/route_packages/packages.js"),
+    journal: path.resolve(__dirname, "src/route_journal/journal.js"),
+    contact: path.resolve(__dirname, "src/route_contact/contact.js"),
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
     assetModuleFilename: ({ filename }) => {
       var dirname = path.dirname(filename);
-      dirname = dirname.replace('src/', '')
+      dirname = dirname.replace("src/", "");
       return `${dirname}/[name][ext]`;
     },
-    clean: true
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-        exclude: /node_modules/
+        use: ["style-loader", "css-loader", "sass-loader"],
+        exclude: /node_modules/,
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|webp|ttf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(mp4|MP4)$/,
-        use: 'file-loader?name=videos/[name].[ext]',
+        use: "file-loader?name=videos/[name].[ext]",
       },
-    ]
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
+      filename: "[name].[contenthash].css",
     }),
     new HtmlWebpackPlugin({
-      title: 'index app',
-      template: './src/route_home/index.html',
-      filename: 'index.html',
-      chunks: ['index'],
+      title: "index app",
+      template: "./src/route_home/index.html",
+      filename: "index.html",
+      chunks: ["index"],
       inject: true,
     }),
     new HtmlWebpackPlugin({
-      title: 'about app',
-      template: './src/route_about/about.html',
-      filename: 'about.html',
-      chunks: ['about'],
+      title: "about app",
+      template: "./src/route_about/about.html",
+      filename: "about.html",
+      chunks: ["about"],
       inject: true,
     }),
     new HtmlWebpackPlugin({
-      title: 'testimonies',
-      template: './src/route_testimonies/testimonies.html',
-      filename: 'testimonies.html',
-      chunks: ['testimonies'],
+      title: "testimonies",
+      template: "./src/route_testimonies/testimonies.html",
+      filename: "testimonies.html",
+      chunks: ["testimonies"],
       inject: true,
     }),
     new HtmlWebpackPlugin({
-      title: 'packages',
-      template: './src/route_packages/packages.html',
-      filename: 'packages.html',
-      chunks: ['packages'],
+      title: "packages",
+      template: "./src/route_packages/packages.html",
+      filename: "packages.html",
+      chunks: ["packages"],
       inject: true,
     }),
     new HtmlWebpackPlugin({
-      title: 'journal',
-      template: './src/route_journal/journal.html',
-      filename: 'journal.html',
-      chunks: ['journal'],
+      title: "journal",
+      template: "./src/route_journal/journal.html",
+      filename: "journal.html",
+      chunks: ["journal"],
       inject: true,
     }),
     new HtmlWebpackPlugin({
-      title: 'contact',
-      template: './src/route_contact/contact.html',
-      filename: 'contact.html',
-      chunks: ['contact'],
+      title: "contact",
+      template: "./src/route_contact/contact.html",
+      filename: "contact.html",
+      chunks: ["contact"],
       inject: true,
-    })
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./sitemap.xml", to: "./" }],
+    }),
   ],
   optimization: {
-    runtimeChunk: 'single',
-    minimizer: [
-      new TerserPlugin(),
-      new OptimizeCssAssetsPlugin(),
-    ],
+    runtimeChunk: "single",
+    minimizer: [new TerserPlugin(), new OptimizeCssAssetsPlugin()],
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, "dist"),
     },
     port: 3000,
     open: true,
@@ -120,6 +124,6 @@ module.exports = {
   performance: {
     hints: false,
     maxEntrypointSize: 512000,
-    maxAssetSize: 512000
-  }
+    maxAssetSize: 512000,
+  },
 };
